d56b51f5eafef94398231c71742d926100aa6c94
Mauricio <rodriguez.mauricioe@gmail.com>
2021-11-08 01:11:57 -0300

    model vehiculo fix

--- api/models.py
+++ api/models.py
@@ -1,5 +1,15 @@
 from django.db import models
+from django.urls import reverse
 
 
 class ModelVehiculo(models.Model):
-    kms_recorridos = models.IntegerField()
+
+    class Meta:
+        verbose_name = _("modelvehiculo")
+        verbose_name_plural = _("modelvehiculos")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("modelvehiculo_detail", kwargs={"pk": self.pk})
