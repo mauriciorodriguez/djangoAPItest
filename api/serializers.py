7cceb401c92e257a4cc93fb78bc03b1ec669f07c
Mauricio <rodriguez.mauricioe@gmail.com>
2021-11-23 14:46:10 -0300

    "id" agregado a api/serializers.py ya que serializers.HyperlinkedModelSerializer no lo muestra por defecto aunque se utilice fields = "__all__"

--- api/serializers.py
+++ api/serializers.py
@@ -22,18 +22,23 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class VehiculoSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+
     class Meta:
         model = Vehiculo
         fields = "__all__"
 
 
 class RecorridoSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+
     class Meta:
         model = Recorrido
         fields = "__all__"
 
 
 class PerfilSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
     usuario = UserSerializer
 
     class Meta:
@@ -42,84 +47,112 @@ class PerfilSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class SesionSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+
     class Meta:
         model = Sesion
         fields = "__all__"
 
 
 class CheckpointSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+
     class Meta:
         model = Checkpoint
         fields = "__all__"
 
 
 class ComponenteSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+
     class Meta:
         model = Componente
         fields = "__all__"
 
 
 class TipoDispositivoSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+
     class Meta:
         model = Tipo_dispositivo
         fields = "__all__"
 
 
 class GeneroSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+
     class Meta:
         model = Genero
         fields = "__all__"
 
 
 class ComponenteParametroSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+
     class Meta:
         model = Componente_parametro
         fields = "__all__"
 
 
 class VehiculoComponenteSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+
     class Meta:
         model = Vehiculo_componente
         fields = "__all__"
 
 
 class TipoDatoSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+
     class Meta:
         model = Tipo_dato
         fields = "__all__"
 
 
 class TipoComponenteSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+
     class Meta:
         model = Tipo_componente
         fields = "__all__"
 
 
 class ParametroTipoSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+
     class Meta:
         model = Parametro_tipo
         fields = "__all__"
 
 
 class ClasificacionComponenteSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+
     class Meta:
         model = Clasificacion_componente
         fields = "__all__"
 
 
 class ClasificacionGrupoComponenteSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+
     class Meta:
         model = Clasificacion_grupo_componente
         fields = "__all__"
 
 
 class TipoVehiculoSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+
     class Meta:
         model = Tipo_vehiculo
         fields = "__all__"
 
 
 class CheckpointComponenteSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+
     class Meta:
         model = Checkpoint_componente
         fields = "__all__"
